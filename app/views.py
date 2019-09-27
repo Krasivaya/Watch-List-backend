ff69399c169375d8b0286eee35ae96de21b1c3c2
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-09-27 21:16:18 +0200

    Add dynamic route

--- app/views.py
+++ app/views.py
@@ -6,3 +6,7 @@ from app import app
 def index():
     message = 'Hello world!'
     return render_template('index.html', message = message)
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    return render_template('movie.html', id = movie_id)
