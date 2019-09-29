e932c1e954e5e468c0e19f694e7d9551f6531808
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-09-29 14:53:03 +0200

    Add more functions to get more movie categories

--- app/views.py
+++ app/views.py
@@ -6,9 +6,11 @@ from .request import get_movies
 @app.route('/')
 def index():
     popular_movies = get_movies('popular')
+    upcoming_movies = get_movies('upcoming')
+    now_showing_movies = get_movies('now_playing')
     title = 'Home - Welcome To The Best Movie Review webiste Online'
     message = 'Welcome To The Best Movie Review webiste Online'
-    return render_template('index.html', title = title, message = message, popular = popular_movies)
+    return render_template('index.html', title = title, message = message, popular = popular_movies, upcoming = upcoming_movies, now_showing = now_showing_movies)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
