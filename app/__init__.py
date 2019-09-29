59f61231114fe5f13f1910e8b47d0154cdd134c2
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-09-29 14:45:46 +0200

    Add instance relative config to connect with instance

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from .config import DevConfig
 
 #Initializing application
-app = Flask(__name__)
+app = Flask(__name__, instace_relative_config = True)
 
 #Setup app configuration
 app.config.from_object(DevConfig)
