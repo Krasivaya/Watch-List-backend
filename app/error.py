d0d4e8af2b5a2df2b00d7a32e0fe11b3185abe2a
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-09-30 11:21:35 +0200

    bg(01) keyerror

--- app/error.py
+++ app/error.py
@@ -4,4 +4,4 @@ from app import app
 #Error view
 @app.errorhandler(404)
 def four_ow_four(error):
-    return render_template('four_ow_four.html'),404
+    return render_template('fourOwfour.html'),404
