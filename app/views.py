25d6f1b5f584c539a95365ba3985b105337850ab
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-09-30 10:24:14 +0200

    Import the function and update movie view function

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_movies, get_movie
 
 #Views
 @app.route('/')
@@ -14,5 +14,6 @@ def index():
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
-    title = 'Movie'
-    return render_template('movie.html', title = title, id = movie_id)
+    movie = get_movie(id)
+    title = f'{movie.title}'
+    return render_template('movie.html', title = title, movie = movie)
