ebe127ca498d29d3f6840a63644fb3e3ad6d9376
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-02 13:16:57 +0200

    Initialize migrate

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager, Server
 from app.models import User, Role
+from  flask_migrate import Migrate, MigrateCommand
 
 #Creating app instance
 app = create_app('development')
@@ -14,6 +15,10 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests) 
+
+#Initialize Migrate
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 #Creating app shell
 @manager.shell
