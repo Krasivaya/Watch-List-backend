34e3160e5afaac7ceb5ed553c9df3f46d4a1cfbe
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-02 15:48:52 +0200

    App login

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from wtforms import ValidationError
 from ..models import User
 
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+    
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
     username = StringField('Enter your username',validators = [Required()])
