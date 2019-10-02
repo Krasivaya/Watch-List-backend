c0203ff223b330f45394ddbd218ec0b35dbd57b3
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-02 09:05:07 +0200

    Bg(03) - 500

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required
 
-def ReviewForm(FlaskForm):
+class ReviewForm(FlaskForm):
     title = StringField('Movie title', validators = [Required()])
     review = TextAreaField('Moview review', validators = [Required()])
     submit = SubmitField('Submit')
