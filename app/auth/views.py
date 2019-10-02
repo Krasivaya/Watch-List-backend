b065fcbd2edd67c2f8669e8f379c7d2c86f40e27
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-02 18:29:28 +0200

    Resolve Bg(02)

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash,request
 from flask_login import login_user,logout_user,login_required
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from .. import db
 from . import auth
 
@@ -32,7 +32,7 @@ def login():
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
 
-        flash('Invalid username or Password')
+        flash('Invalid email or Password')
 
     title = "watchlist login"
     return render_template('auth/login.html',login_form = login_form,title=title)
