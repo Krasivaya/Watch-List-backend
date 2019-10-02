b157a694319b37afa9e02ee0bc6c851bc77cb254
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-02 08:49:01 +0200

    Add new review view

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from app import app
 from .request import get_movies, get_movie, search_movie
-from .models import review
+from .models import reviews
 from .forms import ReviewForm
-Review = review.Review
+Review = reviews.Review
 
 #Home view
 @app.route('/')
