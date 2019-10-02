5323f6215be6edfe359cbcf9cf32cdcee6b79e64
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-02 18:51:13 +0200

    Add Upload profile option

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Config:
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:wecode@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
 
 class ProdConfig(Config):
     pass
